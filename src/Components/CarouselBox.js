57ce8b4c82eb5e766ae0598a0857f1c3aabfc633
Евгений Митюля <evgenni2004@gmail.com>
2022-12-13 16:06:16 +0300

    second commit

---
+++ src/Components/CarouselBox.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import zamok from '../assets/zamok.jpg';
+import zamok2 from '../assets/zamok2.jpg';
+
+class CarouselBox extends Component {
+    render() {
+        return (
+            <Carousel>
+                <Carousel.Item>
+                    <img 
+                        className='d-block w-100'
+                        src={zamok}
+                        alt="zamok"
+                    />
+                    <Carousel.Caption>
+                        <h3>Minsk</h3>
+                        <p>Тут могла быть ваша реклама</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img 
+                        className='d-block w-100'
+                        src={zamok2}
+                        alt="zamok2"
+                    />
+                    <Carousel.Caption>
+                        <h3>Minsk 123</h3>
+                        <p>Тут могла быть 3asdasdваша реклама</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        );
+    }
+}
+
+export default CarouselBox;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,41 +1,62 @@
-import React, { Component } from 'react';
-import { Button, Container, FormControl, Navbar } from 'react-bootstrap';
-import logo from './logo192.png'
+import React, { Component } from "react";
+import {
+  Button,
+  Container,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+} from "react-bootstrap";
+
+import logo from "./logo192.png";
+import { BrowserRouter, Route, Routes, Link} from "react-router-dom";
+
+import Home from "../Pages/Home";
+import Authors from "../Pages/Authors";
+import About from "../Pages/About";
 
 class Header extends Component {
-    render() {
-        return (
-            <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="/">
-                        <img 
-                            src={logo}
-                            height="30"
-                            width="30"
-                            className="d-inline-block align-top"
-                            alt="logo"
-                        />
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.link href="/"> Home </Nav.link>
-                            <Nav.link href="/about"> About us </Nav.link>
-                            <Nav.link> Home </Nav.link>
-                        </Nav>
-                        <Form inline>
-                            <FormControl
-                                type="text"
-                                placeholder="Search"
-                                className="mr-sm-2"
-                            />
-                            <Button variant="outline-info">Search</Button>
-                        </Form>
-                    </Navbar.Collapse>
-                 </Container>
-            </Navbar>
-        );
-    }
+  render() {
+    return (
+      <>
+        <Navbar
+          collapseOnSelect
+          expand="md"
+          bg="dark"
+          variant="dark"
+        >
+          <Container>
+            <Navbar.Brand href="/">
+              <img
+                src={logo}
+                height="30"
+                width="30"
+                className="d-inline-block align-top"
+                alt="logo"
+              />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="/"> Home </Nav.Link>
+                <Nav.Link href="/authors"> Authors </Nav.Link>
+                <Nav.Link href="/about"> About us </Nav.Link>
+              </Nav>
+              <Form inline>
+                <FormControl
+                  type="text"
+                  placeholder="Search"
+                  className="me-sm-2"
+                />
+              </Form>
+              <Button variant="outline-info">Search</Button>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+      </>
+    );
+  }
 }
 
 export default Header;
